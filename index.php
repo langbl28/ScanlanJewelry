a96bb11cddf4cfdef5e2080b5e246c653b9d4252
langbl28 <58338925+langbl28@users.noreply.github.com>
2022-09-26 21:34:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php $title="Scanlan";
+include ($_SERVER['DOCUMENT_ROOT'] . '/_assets/template/head.php');
+include ($_SERVER['DOCUMENT_ROOT'] . '/_assets/template/header.html');
+include ($_SERVER['DOCUMENT_ROOT'] . '/_assets/template/slider.html'); ?>
+   
+    <div id="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Family Owned Since 1903</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>At Scanlan Jewelers, we feature a wide selection of jewelry to choose from whether you are buying a gift for a special someone or splurging on yourself. We look forward to giving you the quality service and personal attention you deserve when looking for that perfect piece. For over one hundred years, Scanlan Jewelers has been providing a family tradition of supreme customer service and exquisite and unique pieces. Call us today to learn more about our inventory or to set up an appointment to discuss creating a custom jewelry piece.</p>
+                    
+                    <p>Come in to see our selection of clocks and watches as well!</p>
+                </div>
+
+                <style>
+                        .subnav{
+                            background-color: #a19367;
+                            padding: 15px;
+                            border: #fff solid 5px;
+                            margin: 0;
+                            padding: 15px;
+                            list-style:none;}
+                        .subnav p, .subnav h5{
+                            color: #fff;}
+                        .subnav-container{
+                            background-color: #a19367;
+                            padding: 15px;}
+                        .subnav li{
+                            padding: 5px 0;}
+                        .subnav a{
+                            text-decoration: none;
+                            color: #fff;}
+                        .subnav a:hover{
+                            text-decoration: underline;}
+                    </style>
+                    <div class="col-md-6">
+                    <div class="subnav-container">
+                        <div class="subnav">
+                            <h5>A Family You Can Trust</h5>
+                            <p>We are here to help you find the right jewelry to fit within your budget. We also have a goldsmith! Perfect for any wear and tear your current jewelry may have endured over the years. Stop in to see what we can do for you. We look forward to meeting you!</p>
+                        </div>
+                    </div>
+                        </div>
+
+
+            </div>
+        </div>
+    </div>
+    <div id="products">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                <a href="https://scanlanjewelerswi.jewelershowcase.com/"><img src="/_assets/img/shortcut1.jpeg"></a>
+                    <h4>SHOP LOOSE DIAMOND</h4>
+                </div>
+                <div class="col-md-3">
+                <a href="https://scanlanjewelerswi.jewelershowcase.com/"><img src="/_assets/img/shortcut12.jpeg"></a>
+                    <h4>SHOP ENGAGEMENT</h4>
+                </div>
+                <div class="col-md-3">
+                <a href="https://scanlanjewelerswi.jewelershowcase.com/"><img src="/_assets/img/shortcut13.jpg"></a>
+                    <h4>SHOP JEWELRY</h4>
+                </div>
+                <div class="col-md-3">
+                <a href="https://www.tciconnection.com/internetApp/app?key=35b6f97fa7d57a39e5a8bb2b043304f287"><img src="/_assets/img/shortcut14.jpeg"></a>
+                    <h4>APPLY FOR FINANCING</h4>
+                </div>                
+</div>
+</div>
+</div>
+                
+    
+    </div> 
+    <div id="find-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Our Location</h2>
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-md-12">
+                    <div class="contact">
+                        <div class="col-md-6">
+                            <h4>Green Bay</h4>
+                            <p>2304 Lineville Road 109<br/>
+                            Suamico, WI 54313<br />
+                            <a href="tel:19204659829">920.465.9829</a><br />
+                            <a href="mailto:scanlanjewelers1903@gmail.com">scanlanjewelers1903@gmail.com</a><br/>
+                            <a href="https://www.facebook.com/scanlanjewelerswi/"><i class="fa fa-facebook-official" aria-hidden="true"></i> Connect with Us!</a></p>
+                        </div>
+                        <div class="col-md-6">
+                            <table class="hours">
+                                <tbody><tr><td>MON</td><td>10 am - 5:00 pm</td></tr>
+                                <tr><td>TUE</td><td>10 am - 6:00 pm</td></tr>
+                                <tr><td>WED</td><td>10 am - 6:00 pm</td></tr>
+                                <tr><td>THU</td><td>10 am - 6:00 pm</td></tr>
+                                <tr><td>FRI</td><td>10 am - 5:00 pm</td></tr>
+                                <tr><td>SAT</td><td>10 am - 4:00 pm</td></tr>
+                                <tr><td>SUN</td><td>Closed</td></tr>
+                            </tbody></table>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>
+<?php include ($_SERVER['DOCUMENT_ROOT'] . '/_assets/template/footer.html'); ?>
